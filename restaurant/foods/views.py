0e04ed08c04511a47d5c1ced46364f569b096bf5
mohammad <mohammadtohidi.mtn@gmail.com>
2022-11-27 12:43:42 -0800

    add url to project

--- restaurant/foods/views.py
+++ restaurant/foods/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Foods
 # Create your views here.
+
+def food_list(request):
+    food_list = Foods.objects.all()
+    context= {
+        "foods" : food_list
+              }
+    
+    return render (request,'foods/list.html',context)
