9c49a81199bd6f37c59f6342105558ca34d95cdb
mohammad <mohammadtohidi.mtn@gmail.com>
2022-12-05 22:26:42 -0800

    detial template

--- restaurant/foods/models.py
+++ restaurant/foods/models.py
@@ -4,6 +4,12 @@ from django.utils.translation import gettext as _
 
 
 class Foods(models.Model):
+    FOOD_TYPE=[
+        ('drinks','نوشیدنی'),
+        ('dinner','صبحانه'),
+        ('lunch','غذای اصلی'),
+    ]
+    
     name  = models.CharField(_('اسم'), max_length=50)
     info  = models.CharField(_('توضیحات'),max_length=100)
     rate  = models.IntegerField (_("امتیاز"),default=0)
@@ -11,7 +17,7 @@ class Foods(models.Model):
     time  = models.IntegerField(_("زمان آماده شدن"))
     date  = models.DateField(_("تاریخ ثبت"),auto_now=False,auto_now_add=True)
     photo = models.ImageField(upload_to='food_images/')
-    
+    type_food = models.CharField(_('نوع خوراکی'),max_length=10,choices=FOOD_TYPE,default='drink')
     def __str__(self):
         return self.name
     
