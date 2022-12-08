a1471b65d0b44ef875ad7b62f3afd16662b9f70a
mohammad <mohammadtohidi.mtn@gmail.com>
2022-12-08 14:38:51 +0330

    Reservation app and model added

---
+++ restaurant/reservation/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.translation import gettext as _
+# Create your models here.
+
+class Reservation(models.Model):
+    name  = models.CharField(_('نام و نام خانوادگی'),max_length=200)
+    email = models.EmailField(_("ایمیل "), max_length=254)
+    phone = models.CharField(_("شماره تلفن"),max_length=15)
+    date  = models.DateField(_("تاریخ"), auto_now=False, auto_now_add=False)
+    time  = models.TimeField(_("زمان"), auto_now=False, auto_now_add=False)
+    number= models.IntegerField(_('تعداد'))
+    
+    def __str__(self):
+        return self.name
+    
--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('foods/',include('foods.urls')),
+    path('book/', include("reservation.urls")),
     
 ]
 
