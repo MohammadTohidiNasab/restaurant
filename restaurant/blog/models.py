b695e83c321285f5cfc819028a4ac55a4b7ecb56
mohammad <mohammadtohidi.mtn@gmail.com>
2022-12-13 22:15:42 +0330

    category model

--- restaurant/blog/models.py
+++ restaurant/blog/models.py
@@ -11,6 +11,15 @@ class Blog(models.Model):
     created_at = models.DateTimeField(_(""), default=timezone.now)
     auther = models.ForeignKey(User, verbose_name=_("نویسنده"), on_delete=models.CASCADE)
     image  = models.ImageField(_("تصویر"), upload_to="blogsImg/")
+    category = models.ForeignKey("Category",related_name='blog', verbose_name=_("دسته بندی"),on_delete=models.PROTECT)
+    def __str__(self):
+        return self.title
     
+    
+class Category(models.Model):
+    title = models.CharField(_("عنوان"), max_length=50)
+    slug  = models.SlugField(_("عنوان انگلیسی"))
+    published_at = models.DateTimeField(_("تاریخ انتشار") ,auto_now=False , auto_now_add=True)
+
     def __str__(self):
         return self.title
