c1742fe0a3e1725406a45fd3ba8bfaf26c7b0c8b
mohammad <mohammadtohidi.mtn@gmail.com>
2022-12-15 14:01:08 +0330

    add template to blog

--- restaurant/blog/urls.py
+++ restaurant/blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+ 
+app_name = 'blog'
+
 urlpatterns = [
-    path('',views.blog_page,name='blog')
+    path('',views.blog_page,name='blog'),
+    path("<int:id>", views.blog_detail, name="blog_detail"),
 ]
--- restaurant/blog/views.py
+++ restaurant/blog/views.py
@@ -3,4 +3,18 @@ from . models import Blog
 # Create your views here.
 
 def blog_page (request):
-    return render(request,'blog.html')
+    blogs = Blog.objects.all()
+    context = {
+        'blogs':blogs
+    }
+    return render(request,'blog.html',context)
+
+
+
+def blog_detail(request,id):
+    blog = Blog.objects.get(id=id)
+    context = {
+        'blog':blog
+    }
+    
+    return render(request,'blog_details.html',context)
